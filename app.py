aea37ddb6c15ee51cfce46e54f6473a7d42a4755
Harshit <mishraharshit111@gmail.com>
2024-08-16 01:32:58 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, jsonify, request
 from weather_scheduler import fetch_current_data, start_scheduler
 from pymongo import MongoClient
@@ -106,43 +105,3 @@ if __name__ == "__main__":
     app.json_encoder = MongoJSONEncoder  
     start_scheduler() 
     app.run(debug=False)
-=======
-from flask import Flask, render_template, request, jsonify
-import requests
-from config import OPENWEATHER_API_KEY
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/weather', methods=['POST'])
-def weather():
-    data = request.get_json()
-    city = data.get('city')
-    print(city)
-    if city:
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric'
-        response = requests.get(url)
-        weather_data = response.json()
-
-        if response.status_code == 200:
-            data = {
-                'city': weather_data['name'],
-                'main':weather_data['weather'][0]['main'],
-                'temperature': weather_data['main']['temp'],
-                'feels_like':weather_data['main']['feels_like'],
-                'description': weather_data['weather'][0]['description'],
-                'icon': weather_data['weather'][0]['icon'],
-                'date':weather_data.get('dt')
-            }
-            print(data)
-            return jsonify(data)
-        else:
-            return jsonify({'error': weather_data.get('message', 'City not found')}), 404
-    return jsonify({'error': 'City not specified'}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 6b0e34fe17c1c762f8e27d34b69521dc778bfcfa
--- frontend/myapp/src/components/weatherAnalytics/weatherAnalytics.js
+++ frontend/myapp/src/components/weatherAnalytics/weatherAnalytics.js
@@ -1,52 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // // import GraphBar from '../BarGraph/GraphBar';
-import LineGraph from "../LineGraph/LineGraph"
-<<<<<<< HEAD
+import LineGraph from "../LineGraph/LineGraph";
+
 // import data from "./data.json";
 import "./weatherAnalytics.css";
-import axios from 'axios';
-=======
-import data from "./data.json";
-import "./weatherAnalytics.css";
+import axios from "axios";
 
->>>>>>> 6b0e34fe17c1c762f8e27d34b69521dc778bfcfa
-
-const WeatherAnalytics = ({location}) => {
+const WeatherAnalytics = ({ location }) => {
   const [weatherData, setWeatherData] = useState([]);
   const [interval, setInterval] = useState(7); // Default to past 7 days
 
-<<<<<<< HEAD
   // useEffect(() => {
   //   const fetchWeatherData = async () => {
   //     try {
   //       // const response = await fetch(`https://api.example.com/weather/interval?days=${interval}`); // Replace with your API endpoint
   //       // const data = await response.json();
-=======
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      try {
-        // const response = await fetch(`https://api.example.com/weather/interval?days=${interval}`); // Replace with your API endpoint
-        // const data = await response.json();
-        setWeatherData(data.slice(0, interval));
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
 
-    fetchWeatherData();
-  }, [interval]);
-
-
-    // useEffect(() => {
-  //   const fetchWeatherData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://api.example.com/weather/interval?location=${location}&days=${interval}` // Replace with your actual API endpoint
-  //       );
-  //       const data = await response.json();
-
-  //       // Slice the data based on interval and set the state
->>>>>>> 6b0e34fe17c1c762f8e27d34b69521dc778bfcfa
   //       setWeatherData(data.slice(0, interval));
   //     } catch (error) {
   //       console.error('Error fetching weather data:', error);
@@ -54,45 +23,41 @@ const WeatherAnalytics = ({location}) => {
   //   };
 
   //   fetchWeatherData();
-<<<<<<< HEAD
+
   // }, [interval]);
 
-
-    useEffect(() => {
+  useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         const response = await axios.post(
-          'http://localhost:5000/current-weather', {
-            city : location,
-            days : interval
-          }, {
+          "http://localhost:5000/current-weather",
+          {
+            city: location,
+            days: interval,
+          },
+          {
             headers: {
-              'Content-Type': 'application/json', // Specify the content type
+              "Content-Type": "application/json", // Specify the content type
               // 'Authorization': 'Bearer YOUR_TOKEN_HERE', // If your API requires an authorization token
               // 'Access-Control-Allow-Origin': '*', // Allow all origins (CORS)
               // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', // Methods allowed
               // 'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Headers allowed
               // // Add any other h
-            }
+            },
           }
         );
         console.log(response.data.summaries);
-        
 
         // Slice the data based on interval and set the state
         setWeatherData(response.data);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
     fetchWeatherData();
   }, [location, interval]);
-=======
-  // }, [location, interval]);
->>>>>>> 6b0e34fe17c1c762f8e27d34b69521dc778bfcfa
 
-  
   const handleIntervalChange = (e) => {
     setInterval(e.target.value);
   };
@@ -109,21 +74,10 @@ const WeatherAnalytics = ({location}) => {
           <option value={90}>Past 3 months</option>
         </select>
       </div>
-<<<<<<< HEAD
+
       <LineGraph data={weatherData.summaries} />
-=======
-      <LineGraph data={weatherData} />
->>>>>>> 6b0e34fe17c1c762f8e27d34b69521dc778bfcfa
     </div>
   );
 };
 
 export default WeatherAnalytics;
-
-
-
-
-
-
-
-  
